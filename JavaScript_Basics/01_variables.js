168399610f6c2821228932ad82e0d2d6bd332801
nitin-chauhan-3 <112232240+nitin-chauhan-3@users.noreply.github.com>
2024-08-22 18:08:23 +0000

    conatiner save this

---
+++ JavaScript_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = "0835CS233D16";
+let accountEmail = "pubg@bgmi.com";
+var accountCourse = "CSE";
+accountGender = "Male";
+let accountTown;
+// accountId = 4;
+
+/*
+Prefer not to use Var in modern JS cause of Block Scope and Functional Scope
+*/
+console.table([accountId, accountEmail,accountCourse,accountGender, accountTown])
