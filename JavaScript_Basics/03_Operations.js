de8ef22555385b955834646b5cba8062da79c631
nitin-chauhan-3 <112232240+nitin-chauhan-3@users.noreply.github.com>
2024-08-23 05:42:15 +0000

    Operations

---
+++ JavaScript_Basics/03_Operations.js
@@ -0,0 +1,17 @@
+// ***********************Operations*****************//
+let num = 3
+let negnum = -num;
+// console.log(negnum);
+
+// Basics Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(3%2);
+// console.log(2**4);
+
+console.log(+'');
+
+let score = 100;
+score++;
+
+console.log(score);
