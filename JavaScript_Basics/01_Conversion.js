3433d3759fa28bab42736c340bef19a808e6ebd0
nitin-chauhan-3 <112232240+nitin-chauhan-3@users.noreply.github.com>
2024-08-23 05:23:41 +0000

    Conversions

---
+++ JavaScript_Basics/01_Conversion.js
@@ -0,0 +1,34 @@
+let input = 33;
+// console.log(input);
+// console.log(typeof input);
+
+let someString = String(input);
+// console.log(someString);
+// console.log(typeof someString);
+
+let someBoolean = Boolean(input);
+// console.log(typeof someBoolean);
+
+
+let numString = true;
+// console.log(typeof numString); 
+
+let someNum = Number(numString);
+// console.log(someNum);
+// console.log(typeof someNum);
+
+// undefined => NaN;
+// null => 0;
+// true => 1, false => 0;
+// "44nitin"=>NaN = not a number 
+
+let isloggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isloggedIn)
+console.log(booleanisLoggedIn);
+
+
+  
+// "" => false 
+// 1 => true, 0 => false ;
+// String and AnyNumber(0)=>true
