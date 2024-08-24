80a7ade5fb1ccb101a955dd95e772c2e9f40ce1b
nitin-chauhan-3 <112232240+nitin-chauhan-3@users.noreply.github.com>
2024-08-24 05:21:39 +0000

    Numbers

--- JavaScript_Basics/05_Strings.js
+++ JavaScript_Basics/05_Strings.js
@@ -7,9 +7,39 @@ console.log(`My name is ${name} and my repocount is ${repcount}`);
 
 const gameName =  new String("Change")
 
-console.log(gameName.length)
-console.log(gameName.endsWith('e'))
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('n'))
-console.log(gameName.split('n'))
+// console.log(gameName.length)
+// console.log(gameName.endsWith('e'))
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('n'))
+// console.log(gameName.split('n'))
+
+const anotherString = "  Samay  "
+// console.log(anotherString);
+// console.log(anotherString.trim());
+
+const url = "samay.com/samay^&raina"
+// console.log(url.replace('^&','@'));
+
+const TwoString = "Kolkata"
+// console.log(TwoString.substring(0,4));
+// console.log(TwoString.slice(0,4));
+// console.log(TwoString.substring(-6,4)); -ve will not change the output 
+// console.log(TwoString.slice(-6,4)); it counts the -ve numbering
+
+const WebName = "Nitin/Bgmi/Player"
+console.log(WebName.split('/'));
+// console.log(WebName.split(' '));
+
+
+const contentString = "Hello, world!";
+const contentString2 = "How Are You"
+
+console.log(contentString.blink());
+
+console.log(contentString.concat(' ',contentString2));
+console.log(contentString.bold("World"));
+
+
+console.log(contentString.fontcolor("Red"));
+console.log(contentString.fontsize(9));
 
---
+++ JavaScript_Basics/06_Num-and-math.js
@@ -0,0 +1,16 @@
+const balance = new Number(100)
+// console.log(typeof balance);
+
+console.log(balance);
+
+// console.log(balance.toString().concat(' reaper'));
+// console.log(balance.toFixed(3)); // This method for Representing Precision values mostly in E-commerce sites 
+
+const oneNum = new Number(99233.544499)
+
+// console.log(oneNum.toPrecision(3));
+
+const crores = 1000000000
+
+console.log(crores.toLocaleString('en-IN'));
+console.log(crores.toLocaleString());
